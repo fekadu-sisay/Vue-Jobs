a94bac003716f0189064cb807b3472b9ced7a048
fekadusisay21 <132390088+fekadusisay21@users.noreply.github.com>
2024-08-17 15:07:07 +0300

    adding remote data

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://jobs-72de.onrender.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
